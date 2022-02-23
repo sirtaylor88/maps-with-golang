4fb3c07febea2a47b592f75b0bdc5ae1687f165b
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2022-02-23 10:24:45 +0100

    Add syntax to iterate over a map.

--- main.go
+++ main.go
@@ -9,11 +9,25 @@ func main() {
 	// 	"blue":  "#0000ff",
 	// }
 
-	colors := make(map[string]string)
+	// colors := make(map[string]string)
 
-	colors["white"] = "#ffffff"
+	// colors["white"] = "#ffffff"
 
-	delete(colors, "white")
+	// delete(colors, "white")
+
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+		"white": "#ffffff",
+	}
 
 	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
